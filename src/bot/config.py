4ef1bb290a18cc0bf4e903142fd04544a75a471d
Smarandii <olegsmarandi@gmail.com>
2025-06-01 00:03:33 +0300

    v0.0.3

--- src/bot/config.py
+++ src/bot/config.py
@@ -1,10 +1,15 @@
-from pydantic import BaseSettings, PostgresDsn
+from pathlib import Path
+
+from pydantic_settings import BaseSettings
+from pydantic import PostgresDsn
+
+project_root_path = Path(__file__).parent.parent.parent
 
 
 class Settings(BaseSettings):
     BOT_TOKEN: str
     DATABASE_URL: PostgresDsn = (
-        "postgresql+asyncpg://postgres:postgres@localhost:5432/tgbot_nakama"
+        "postgresql://postgres:postgres@localhost:5432/tgbot_nakama"
     )
     QDRANT_URL: str = "http://localhost:6333"
 
@@ -13,4 +18,4 @@ class Settings(BaseSettings):
         env_file_encoding = "utf-8"
 
 
-settings = Settings()  # singleton
+settings = Settings(_env_file=project_root_path / ".env")
--- src/bot/logger.py
+++ src/bot/logger.py
@@ -1,6 +1,7 @@
 from loguru import logger
 import os
 from pathlib import Path
+from src.bot.config import project_root_path
 
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
 LOG_PATH = Path(os.getenv("LOG_PATH", "logs"))
@@ -8,7 +9,7 @@ LOG_PATH.mkdir(exist_ok=True)
 
 logger.remove()  # drop default
 logger.add(
-    LOG_PATH / "bot_{time:YYYYMMDD}.log",
+    project_root_path / LOG_PATH / "bot_{time:YYYYMMDD}.log",
     rotation="10 MB",
     retention=3,
     level=LOG_LEVEL,
